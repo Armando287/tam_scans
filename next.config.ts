28c8d809acae4a0e2ca74b664862150e708bfd2c
Armando <56686121+Armando287@users.noreply.github.com>
2026-07-29 14:27:16 -0300

    sfsdfsdf
    sdfsdfsdf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["firebase-admin"],
+  // removed firebase-admin
   images: {
     remotePatterns: [
       {
