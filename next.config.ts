028b557e4ea13b551f63ea7af8d9d725b8d3ad68
Armando <56686121+Armando287@users.noreply.github.com>
2026-07-30 12:04:29 -0300

    fff
    ff

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // removed firebase-admin
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
